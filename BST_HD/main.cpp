9b84e858c503ce1cb815cbceeee1e4e9659f267d
Alexandru Cancescu <Andu@cancescus-macbook-pro-3.local>
2016-06-01 02:43:45 +0300

    Initial Commit

---
+++ BST_HD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BST_HD
+//
+//  Created by Cancescu Alexandru on 01/06/16.
+//  Copyright © 2016 Andu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
